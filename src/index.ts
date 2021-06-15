4a3d727c690a34ac74231d2b3b51db363bc3b982
muhai <muhai.li@tuya.com>
2021-06-15 21:07:26 +0800

    feat: alias picker

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export default (api: IApi) => {
 
   api.chainWebpack((config) => {
     config.resolve.extensions.add('.android.js').add('.ios.js').add('.web.js');
+    config.entry('style').add(path.resolve(__dirname, '../node_modules/rmc-picker/assets/index.css'))
     return config;
   });
 
@@ -49,6 +50,8 @@ export default (api: IApi) => {
           'expo-asset/build/Image/assetPathUtils',
         'react-native/Libraries/Image/resolveAssetSource$':
           'expo-asset/build/resolveAssetSource',
+        // Alias Picker to rmc-picker
+        './exports/Picker': 'rmc-picker'
       },
     };
   });
